95f238f01e48e11c4e43e2761d9374efbbd2c943
2md-mahmudul-hasan <2md.mahmudul.hasan@gmail.com>
2023-08-18 09:28:39 +0600

    menu

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import Cover from '../../Shared/Cover/Cover';
+import coverImg from '../../../assets/menu/banner3.jpg'
+import PopularMenu from '../../Home/PopularMenu/PopularMenu';
 const Menu = () => {
   return (
     <div>
       <Helmet>
         <title>bistroo-boos||menu</title>
       </Helmet>
-      menu
+      <Cover img={coverImg} title="Our menu"></Cover>
+      <PopularMenu></PopularMenu>
+      <Cover img={coverImg} title="Our menu"></Cover>
+      <PopularMenu></PopularMenu>
+      <Cover img={coverImg} title="Our menu"></Cover>
+      <PopularMenu></PopularMenu>
+      <Cover img={coverImg} title="Our menu"></Cover>
+      <PopularMenu></PopularMenu>
     </div>
   );
 };
---
+++ src/pages/Shared/Cover/Cover.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Parallax } from 'react-parallax';
+const Cover = ({ img, title }) => {
+  return (
+    <Parallax
+      blur={{ min: -15, max: 15 }}
+      bgImage={img}
+      bgImageAlt="bgimage"
+      strength={-200}
+    >
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold uppercase">{title}</h1>
+          <p className="mb-5">menu items</p>
+        </div>
+      </div>
+      <div style={{ height: '200px' }} />
+    </Parallax>
+
+  );
+};
+
+export default Cover;
